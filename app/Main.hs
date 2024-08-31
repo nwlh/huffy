569fca65cb65043c3b2f61768fce8d62ad8ed519
newlyghter <stigma-peeling-0r@icloud.com>
2024-08-31 10:57:42 +0200

    frequency sort

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Data.List
+
+sortFreq :: (Ord a, Ord b) => [(a, b)] -> [(a, b)]
+sortFreq [] = []
+sortFreq [x] = [x] 
+sortFreq xs = sortBy (\(_,a) (_,b) -> flip compare a b) xs
+
+analyzeString :: String -> [(Char, Int)]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "PRIORITY QUEUE: "
+  print $ sortFreq [("a", 2), ("b", 5), ("c", 3)]
